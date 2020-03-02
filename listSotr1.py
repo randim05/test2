eec39d30579d675a437b3329854e0a89f59432a4
randim <randim05@mail.ru>
2020-03-02 11:13:00 +0400

    5

--- listSotr1.py
+++ listSotr1.py
@@ -1,3 +1,14 @@
-a = [1, 2, 3, 4, 5]
-a.sort(reverse=True)
-print(a)
+# a = [1, 2, 3, 4, 5]
+# a.sort(reverse=True)
+# print(a)
+
+for i in range(10):
+    print(i, end=" ")
+    for y in "abcdefghijklmnopqrstuvwxyz":
+        if y == 'e':
+            break
+        if y == 'b':
+            continue
+        print(y, end=" ")
+    if i == 3:
+        break

dc9ab6d167be9b1a680ca40c5613b7be46d8e77b
randim <randim05@mail.ru>
2020-08-24 16:03:41 +0400

    6

--- calc3.py
+++ calc3.py
@@ -13,50 +13,39 @@ def valid_assignment(x):
     if len(l) == 2:
         if all_isalpha(l[0]):
             if l[1].isdigit():
-                valid_var[l[0]] = l[1]
+                valid_var[hash(l[0])] = l[1]
                 return l
             elif l[1].isalpha() and all_isalpha(l[1]):
-                if l[1] in valid_var:
-                    valid_var[l[0]] = valid_var[l[1]]
-                    return [l[0], valid_var[l[0]]]
+                if hash(l[1]) in valid_var:
+                    valid_var[hash(l[0])] = valid_var[hash(l[1])]
+                    return [l[0], valid_var[hash(l[0])]]
                 else:
                     print("Invalid identifier")
                     return False
             else:
-                print("Unknown variable")
+                print("Invalid identifier")
                 return False
         else:
             print("Invalid identifier")
             return False
     else:
+        print('Invalid assignment')
         return False
 
 def calc(args):
-    for i in args:
-        i = i.strip()
-    res = int(args[0])
-    for i in range(len(args)):
-        if args[i] == "+":
-            # a = i.index()
-            res += int(args[i+1])
-        elif args[i] == "-":
-            # a = i.index()
-            res -= int(args[i+1])
-    return res
+    # for i in args:
+    #     i = i.strip()
+    # res = int(args[0])
+    # for i in range(len(args)):
+    #     if args[i] == "+":
+    #         # a = i.index()
+    #         res += int(args[i+1])
+    #     elif args[i] == "-":
+    #         # a = i.index()
+    #         res -= int(args[i+1])
+    # return res
+    print(eval(''.join(args)))
 
-
-    # args = list(args)
-    # if args[1] == '+':
-    #     targs = args[0] + args[2]
-    #     args = [targs] + args[3:]
-    #     calc(args)
-    # elif args[1] == '-':
-    #     targs = args[0] - args[2]
-    #     args = [targs] + args[3:]
-    #     calc(args)
-    # print(targs)
-    # exec(''.join(args))
-    # eval(''.join(args))
 
 valid_var = {}
 
@@ -81,15 +70,14 @@ while True:
                 # print(exec(''.join(list_user_oper)))
             out_list = []
             for i in list_user_oper:
-                if i in valid_var:
-                    out_list.append(valid_var[i])
+                if hash(i) in valid_var:
+                    out_list.append(valid_var[hash(i)])
                 else:
                     out_list.append(i)
-            print(calc(out_list))
-        elif user_input.strip() in valid_var:
-            print(valid_var[user_input])
-            # out_res = \
-            # eval(''.join(out_list))
-            # print(out_res)
-    # except:
-    #     pass
+            calc(out_list)
+        elif hash(user_input.strip()) in valid_var:
+            print(valid_var[hash(user_input)])
+        elif user_input == '':
+            continue
+        else:
+            print('Invalid assignment')

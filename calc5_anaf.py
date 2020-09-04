7aea6d88d037dd3f06e7dc15c161ec054d526deb
randim <randim05@mail.ru>
2020-09-05 01:25:24 +0400

    8

--- calc5_anaf.py
+++ calc5_anaf.py
@@ -1,7 +1,8 @@
 
 variable_storage = {}
 
-
+ZNAKS = ("+", "-", "/", "*")
+DIGITS = "1234567890"
 # def block
 
 
@@ -41,46 +42,94 @@ def ansigment_validator(ui):  # --> bool
         return False
 
 
-
 # 3 + 8 * ((4 + 3) * 2 + 1) - 6 / (2 + 1)
 # -3+erwe545634df-)(-3*2
 
 
 def ui_preprocessor(ui):
-    digits = []
-    alphas = []
-    math_zn = []
-    res = []
-    half_moon = []
+    if ui.starstwhith(")") or ui.endswith("("):
+        print("Invalid expression")
+        return False
+    # count ( = )
+    # elif ui.count("(") - ui.count(")") != 0:
+    #     print("Invalid expression")
+    #     return False
+    elif ui[-1] in ZNAKS:           # if ui ends +-*/
+        print("Invalid expression")
+        return False
+
+    # ( ahead ) else Invalid expression
+    was_left_bracket = 1                    # there was no opening bracket
+    counter_left_bracket = 0
     for i in ui:
-        if i.isdigit():  # if coming digit
-            if len(alphas) != 0 or len(math_zn) == 0:  # was alphas and not math_zn
-                return "Invalid variables"  # digit after symbol not valid variable
+        if i == "(":
+            was_left_bracket = 0            # ( ahead )
+            counter_left_bracket += 1
+        if i == ")" and was_left_bracket:  # coming ) and there was no opening bracket
+            print("Invalid expression")
+            return False
+        if i == ")":                        # drop pair brackets
+            counter_left_bracket -= 1
+        if counter_left_bracket <= 0:       # drop all pair brackets
+            was_left_bracket = 1
+
+        # if i == ")" and counter_left_bracket:
+        #     was_left_bracket += 1  # ( ahead )
+        #     counter_left_bracket -= 1
+
+    # temp lists
+    while "**" in ui or \
+          "++" in ui or \
+          "--" in ui or \
+          "+-" in ui or \
+          "-+" in ui or \
+          "\n" in ui or \
+          "\t" in ui:
+        ui = ui.replace("**", "*")
+        ui = ui.replace(" ", "")
+        # ui.replace("/", "//")
+        ui = ui.replace("++", "+")
+        ui = ui.replace("--", "+")
+        ui = ui.replace("+-", "-")
+        ui = ui.replace("-+", "-")
+        ui = ui.replace("\n", "")
+        ui = ui.replace("\t", "")
+
+    digits = []
+    letters = []
+    math_symbols = []
+    bracket = []
+    result = []
+
+    # main parser loop
+    # ui contains only +-*/()1234567890 and letters
+    for i in ui:
+        if i in DIGITS:                                          # if coming digit
+            if len(letters) != 0 or len(math_symbols) == 0:      # was alphas and not math_zn
+                return "Invalid variables"                       # digit after symbol not valid variable
             else:
-                res.append(''.join(alphas))  # coming digit after symbol
-                alphas = []  # drop symbol
-                res.append(''.join(math_zn))  # and after math_zn
-                math_zn = []  # drop math_zn
+                result.append(''.join(letters))  # coming digit after symbol
+                letters = []  # drop symbol
+                result.append(''.join(math_symbols))  # and after math_zn
+                math_symbols = []  # drop math_zn
             digits.append(i)  # and go create next digit
 
         elif i.isalpha():  # if coming alphas
-            if len(digits) != 0 or len(math_zn) == 0:  # a was digits or no math_zn
+            if len(digits) != 0 or len(math_symbols) == 0:  # a was digits or no math_zn
                 return "Invalid expression"  #
             else:
-                res.append(''.join(digits))  # coming digit after symbol
+                result.append(''.join(digits))  # coming digit after symbol
                 digits = []  # drop symbol
-                res.append(''.join(math_zn))  # and after math_zn
-                math_zn = []
-            alphas.append(i)
-        elif i in (' ', '\n', "\t"):
-            continue
+                result.append(''.join(math_symbols))  # and after math_zn
+                math_symbols = []
+            letters.append(i)
 
         elif i in ("+", "-", "/", "*"):
-            math_zn.append(i)
-        elif i == ')' and len(half_moon) == 0:
+            math_symbols.append(i)
+        elif i == ')' and len(bracket) == 0:
             return "Invalid expression"
         elif i == "(":
-            half_moon.append(i)
+            bracket.append(i)
         elif ui.starstwhith(")"):
             return "Invalid expression"
 

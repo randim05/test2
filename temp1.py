3e4d459b8566896e208a6d5c7db28bbdc8b15945
randim05 <randim05@mail.ru>
2020-09-15 15:37:29 +0400

    9

--- temp1.py
+++ temp1.py
@@ -8,6 +8,40 @@
 #
 # print(luhn_test("4000003414116814"))
 # print(int('323.03'))
-res_matrix = [[1, 2], [3, 4]]
-for i in res_matrix:
-    print(" ".join([str(_) for _ in i]), sep=' ')
+
+
+# res_matrix = [[1, 2], [3, 4]]
+# for i in res_matrix:
+#     print(" ".join([str(_) for _ in i]), sep=' ')
+
+
+vowels = {'a', 'e', 'i', 'o', 'u'}
+
+
+def find_apostrophe(word, start):
+    i = word.index("'", start)
+
+    if i == -1:
+        print("1")
+        return -1
+
+    if i == 0:
+        print("2")
+        return find_apostrophe(word, 1)
+
+    elif i == len(word) - 1:
+        print("3")
+        return -1
+
+    else:
+        previous_char = word[i - 1]
+        if previous_char in set(vowels):
+            print("4")
+            return i
+
+        else:
+            print("5")
+            return find_apostrophe(word, i + 1)
+
+correct_index = find_apostrophe("'w'ord'", 0)
+print(correct_index)

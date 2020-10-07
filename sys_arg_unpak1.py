978b1148e20a0c3bae4ce7a75f430dd21da9b6eb
randim05 <randim05@mail.ru>
2020-10-07 19:15:40 +0400

    capuchino milk = 100 not 10

--- sys_arg_unpak1.py
+++ sys_arg_unpak1.py
@@ -6,7 +6,8 @@
 
 import math
 import sys
-args = ["python", "sys_arg_unpak1.py", "--type=diff", "--principal=500000", "--periods=8", "--interest=7.8"]
+# args = ["python", "sys_arg_unpak1.py", "--type=diff", "--principal=500000", "--periods=8", "--interest=7.8"]
+args = ["python", "creditcalc.py", "--type=annuity", "--principal=1000000", "--periods=60", "--interest=10"]
 
 arg_dict = {'type': 0, "payment": 0, "principal": 0, "periods": 0, "interest": 0}
 main_loop_flag = True
@@ -33,7 +34,7 @@ while main_loop_flag:
             try:
                 arg_dict["interest"] = int(i.split("=")[1])
             except ValueError:
-                interest = float(i.split("=")[1])
+                arg_dict["interest"] = float(i.split("=")[1])
             if arg_dict["interest"] <= 0:
                 print("Incorrect parameters")
                 main_loop_flag = False
@@ -63,17 +64,21 @@ while main_loop_flag:
         now_month = 1
         sum_pay = 0
         while now_month <= periods:
-            dif_pay = principal / periods + interest * (principal - (principal * (now_month - 1)) / periods)
+            dif_pay = math.ceil(principal / periods + interest * (principal - (principal * (now_month - 1)) / periods))
             print(f'Month {now_month}: payment is {dif_pay}')
             now_month += 1
             sum_pay += dif_pay
         print()
-        print(f'Overpayment = {sum_pay - principal}')
+        print(f'Overpayment = {int(sum_pay - principal)}')
+        main_loop_flag = False
+        break
     if pay_type == "annuity":
         if payment and periods and interest and not principal:
             principal = payment / ((interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
             print(f'Your loan principal = {int(principal)}!')
-            print(f'Overpayment = {int(principal) - payment * periods}')
+            print(f'Overpayment = {int(payment * periods - principal)}')
+            main_loop_flag = False
+            break
         elif principal and payment and interest and not periods:
             n = math.log(payment / (payment - interest * principal), 1 + interest)
             if n % 10 == 0:
@@ -86,4 +91,15 @@ while main_loop_flag:
                 print("It will take " + str(n) + " months to repay this loan!")
             else:
                 print("It will take " + str(n // 12) + " years and " + str(n % 12) + " months to repay this loan!")
-            print(f'Overpayment = {principal - payment * periods}')
+            print(f'Overpayment = {payment * periods - principal}')
+            main_loop_flag = False
+            break
+        elif principal and periods and interest and not payment:
+            # n = math.log(payment / (payment - periods * principal), 1 + i)
+            aut = principal * ((interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
+            print(f'Your annuity payment = {round(aut)}!')
+            print(f'Overpayment = {round(aut) * periods - principal}')
+
+            main_loop_flag = False
+            break
+
